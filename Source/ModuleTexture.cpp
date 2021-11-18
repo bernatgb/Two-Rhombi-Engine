5da243362e419090f3a40558766b2e8e8f4ae5ed
bernatgb <33035442+bernatgb@users.noreply.github.com>
2021-11-18 17:11:37 +0100

    Init and Update changed.

--- Source/ModuleTexture.cpp
+++ Source/ModuleTexture.cpp
@@ -15,26 +15,34 @@ ModuleTexture::~ModuleTexture()
 
 bool ModuleTexture::Init()
 {
+	ilInit();
+
 	// Load image data with DevIL into CPU
-	//ilLoadImage("..\\Source\\Lenna");
+	lennaTexture;
+	bool lennaLoaded  = ilLoadImage("..\\Source\\Lenna");
 
 	// Create and load OpenGL texture into GPU
-	//glGenTextures(1, &lennaTexture);
-	
-	// Add texture coordinates (UVs) into VBO
-	//glActiveTexture(lennaTexture);
+	glGenTextures(1, &lennaTexture);
+	glBindTexture(GL_TEXTURE_2D, lennaTexture);
+
+
 
 	return true;
 }
 
 update_status ModuleTexture::Update()
 {
+	// Add texture coordinates (UVs) into VBO
+	glActiveTexture(lennaTexture);
+
+	glBindTexture(GL_TEXTURE_2D, lennaTexture);
+
 	return UPDATE_CONTINUE;
 }
 
 bool ModuleTexture::CleanUp()
 {
-	//glDeleteTextures(1, &lennaTexture);
+	glDeleteTextures(1, &lennaTexture);
 
 	return true;
 }
